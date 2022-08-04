f1f1b520f1be766a0f4e344fa89765de62c1400a
Anuj Rouniyar <70853978+Anuj2403@users.noreply.github.com>
2022-08-04 16:22:58 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (90.91%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0){ 
+            p/=2;
+            q/=2;
+        }
+        return 1 - p % 2 + q % 2;
+    }
+};
+
+//Reference --> https://leetcode.com/problems/mirror-reflection/discuss/2376191/C%2B%2B-Java-Python-or-Faster-then-100-or-Full-explanations-or
