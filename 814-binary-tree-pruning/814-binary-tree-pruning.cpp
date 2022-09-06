3b17920208b376eb9c53199d92b359fa8704f793
Anuj Rouniyar <70853978+Anuj2403@users.noreply.github.com>
2022-09-06 22:43:52 +0530

    Time: 3 ms (71.58%), Space: 8.9 MB (21.01%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool canDeleteNode(TreeNode* root)
+    {
+        if(root->left==NULL && root->right==NULL && root->val==0)
+            return true;
+        return false;
+    }
+    
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root==NULL)
+            return NULL;
+        root->left=pruneTree(root->left);
+        root->right=pruneTree(root->right);
+        if(canDeleteNode(root)) return NULL;
+        return root;
+    }
+};
